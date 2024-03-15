cf08e4c227754a4e3d8eb42a0c177dc0d5527140
kshoester <156963490+kshoester@users.noreply.github.com>
2024-03-14 22:37:13 -0400

    commit

---
+++ script.js
@@ -0,0 +1,41 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3Nob2VzdGVyIiwiYSI6ImNsdG9jOXN3djBoMnYyaW1zYnRuZ3VkYzYifQ.Z976OphNTmOc_8gG7O6khQ';
+
+const map = new mapboxgl.Map({
+    container: 'my-map',
+    style: 'mapbox://styles/kshoester/cltqs99fy034801qedunp7lji',
+    center: [-79.39, 43.66],
+    zoom: 12,
+});
+
+map.on('load', () => {
+    map.addSource('uoft-data', {
+        type: 'geojson',
+        data: {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'feature',
+                    'properties': {
+                        'name': 'Sidney Smith Hall'
+                    },
+                    'geometry': {
+                        'coordinates': [-79.39865237301687,
+                            43.662343395037766
+                        ],
+                        'type': 'Point'
+                    }
+                }
+            ]
+        }
+    });
+
+    map.addLayer({
+        'id': 'uoft-pnt',
+        'type': 'circle',
+        'source': 'uoft-data',
+        'paint': {
+            'circle-radius': 6,
+            'circle-color': '#B42222'
+        }
+    });
+});
